a78a856dccff3aebcd5efabd2c231bb1f9ac0cdc
Chingizkhan <chingizkhan-c@auto.kz>
2024-02-24 00:01:00 +0600

    Feature:  - connected account and user tables

--- db/migrations/000001_init.up.sql
+++ db/migrations/000001_init.up.sql
@@ -1,12 +1,13 @@
 CREATE TABLE IF NOT EXISTS users(
     id uuid PRIMARY KEY default gen_random_uuid(),
     name varchar(30),
-    login varchar(20) NOT NULL,
-    email varchar(20),
+    login varchar(30) UNIQUE NOT NULL,
+    email varchar(30) UNIQUE,
     email_verified bool DEFAULT FALSE,
     password_hash varchar(100) NOT NULL,
-    phone varchar(15),
+    phone varchar(15) UNIQUE,
     roles text NOT NULL,
+    password_changed_at timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
     created_at timestamp(6) NOT NULL default current_timestamp(6),
     updated_at timestamp(6) NOT NULL default current_timestamp(6)
 );
